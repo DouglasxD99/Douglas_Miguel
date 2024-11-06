779956e9f059d0a00702ed13d72e165da718b2f0
DouglasxD99 <159488948+DouglasxD99@users.noreply.github.com>
2024-11-06 10:39:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'matemática',
+    'Quanto é 2x3?',
+    '6 e a resposta'
+)
+criaCartao(
+    'Língua Francesa',
+    'Como se diz Bom dia em françes?',
+    'Bonjur'
+)
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Inglaterra?',
+    'A capital é Londres'
+)
+criaCartao(
+    'História',
+    'Em qual ano começou a Segunda grande guerra?',
+    'Foi entre 1940 até 1944'
+)
+criaCartao(
+    'Geogafria',
+    'Onde fica Caracas?',
+    'Caracas fica na Venezuela'
+)
+criaCartao(
+    'Geografia',
+    'O que signifia URSS?',
+    'União das Repúblicas Socialistas Sovieticas'
+)
